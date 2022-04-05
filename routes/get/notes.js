9796b3b67bc495b21a2e2d226b04c0566c6aeb4d
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2022-04-05 15:13:10 +0200

    Possible bugfix

--- routes/get/notes.js
+++ routes/get/notes.js
@@ -4,14 +4,17 @@ const db = require("better-sqlite3")(path.join(__dirname, "../../kigin.db"));
 module.exports = (req, res) => {
     const key = req.header("authorization");
 
+    // Check if key (syntax-wise) is valid
     if (!key) return res.send({ status: "NO_KEY" });
     if (key.length !== 289 || key.charAt(32) !== ".") return res.send({ status: "KEY_INCORRECT" });
 
+    // Get results
     const results = db.prepare("SELECT id FROM notes WHERE ownedBy = ? ORDER BY createdAt DESC;").all([key.split(".")[0]]);
 
+    // Put all the IDs in an ID array
     let ids = [];
 
     for (const item of results) ids.push(item.id);
-
+    
     return res.send({ status: "SUCCESS", result: ids });
 }
